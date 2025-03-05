5e5d9550757c9b51e17b1aa78b412023376e5c2b
Parmeet Kumar <parmeetkumarg90@gmail.com>
2025-03-05 20:49:04 +0530

    Update Stopwatch.js

--- Stopwatch.js
+++ Stopwatch.js
@@ -1,48 +1,50 @@
-var ti, newn, h = "0" + 0, m = "0" + 0, s = "0" + 0, flag = 1;
-function start() {
-    if (flag == 1) {
-        newn = setInterval(
-            function () {
-                document.getElementById('now').innerHTML = h + " : " + m + " : " + s;
-                s++;
-                s = s < 10 ? ("0" + s) : s;
-                if (s > 60) {
-                    m++;
-                    s = 0;
-                    m = m < 10 ? ("0" + m) : m;
-                }
-                if (m > 60) {
-                    h++;
-                    m = 0;
-                    h = h < 10 ? ("0" + h) : h;
-                }
-            }, 1000
+    let ti, newn, h = "0" + 0, m = "0" + 0, s = "0" + 0, flag = 1;
+    function start() {
+        if (flag == 1) {
+            document.getElementById('error').innerHTML = "";
+            newn = setInterval(
+                function () {
+                    document.getElementById('now').innerHTML = h + " : " + m + " : " + s;
+                    s++;
+                    s = s < 10 ? ("0" + s) : s;
+                    if (s > 60) {
+                        m++;
+                        s = 0;
+                        m = m < 10 ? ("0" + m) : m;
+                    }
+                    if (m > 60) {
+                        h++;
+                        m = 0;
+                        h = h < 10 ? ("0" + h) : h;
+                    }
+                }, 1000
 
-        );
-        flag = 0;
+            );
+            flag = 0;
+        }
+        else {
+            document.getElementById('error').innerHTML = "Already Running";
+        }
     }
-    else {
-        document.getElementById('error').innerHTML = "Already Working";
+    function stop() {
+        if (flag == 1) {
+            document.getElementById('error').innerHTML = "Already Stopped";
+        }
+        else {
+            document.getElementById('error').innerHTML = "";
+            clearInterval(newn);
+            flag = 1;
+        }
     }
-}
-function stop() {
-    if (flag == 1) {
-        document.getElementById('error').innerHTML = "Already Stopped";
+    function reset() {
+        s = 0 + "0";
+        h = 0 + "0";
+        m = 0 + "0";
+        document.getElementById('lapbox').innerHTML = "";
     }
-    else {        
-        document.getElementById('error').innerHTML = "";
-        clearInterval(newn);
-        flag = 1;
+    function lap() {
+        const lapdata = document.createElement('p');
+        lapdata.innerText = h + ":" + m + ":" + s;
+        document.getElementById('lapbox').appendChild(lapdata);
+        // console.log(h + ":" + m + ":" + s);
     }
-}
-function reset() {
-    s = 0 + "0";
-    h = 0 + "0";
-    m = 0 + "0";
-    document.getElementById('now').innerText = 'Start Me Again';
-}
-function lap() {
-    let li = document.createElement('li');
-    li.innerText = h + " : " + m + " : " + s;
-    document.getElementsByTagName('ol')[0].append(li);
-}
